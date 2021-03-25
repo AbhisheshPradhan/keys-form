ee4ae7bcc585913710b11535a01f35b1f2428524
AbhisheshPradhan <pradhan.abhishesh@gmail.com>
2021-03-26 08:50:49 +1100

    .

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ function App() {
           validate={validate}
           render={({ handleSubmit, reset, submitting, pristine, values }) => (
             <form onSubmit={handleSubmit} noValidate>
-              <Paper style={{ padding: 16 }}>
+              <Paper style={{ padding: 16, boxShadow: 'none' }}>
                 <Stepper activeStep={0} alternativeLabel>
                   {steps.map((label) => (
                     <Step key={label}>
@@ -99,7 +99,8 @@ function App() {
                 <div align='center'>
                   <img src='about_family.png' />
                 </div>
-                <Grid container alignItems='flex-start' spacing={2}>
+
+                <Grid container alignItems='flex-start' spacing={2} style={{ marginBottom: 15 }}>
                   <Grid item xs={12} md={12}>
                     <FormControl component='fieldset'>
                       <FormGroup col>
@@ -138,7 +139,9 @@ function App() {
                       aria-controls='panel1d-content'
                       id='panel1d-header'
                     >
-                      <Typography>Adult 1:</Typography>
+                      <Typography variant='h5' component='h4'>
+                        Adult 1:
+                      </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                       <Grid container alignItems='flex-start' spacing={2}>
@@ -464,8 +467,344 @@ function App() {
                     </AccordionDetails>
                   </Accordion>
 
+                  <Accordion
+                    expanded={adultExpanded === 'panel2'}
+                    onChange={handleChange('panel2')}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls='panel1d-content'
+                      id='panel1d-header'
+                    >
+                      <Typography variant='h5' component='h4'>
+                        Adult 2:
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Grid container alignItems='flex-start' spacing={2}>
+                        <Grid item xs={12} md={6}>
+                          <FormControl component='fieldset'>
+                            <FormLabel>
+                              <Typography variant='h5' component='h5'>
+                                Primary
+                              </Typography>
+                            </FormLabel>
+                            <RadioGroup row>
+                              <FormControlLabel
+                                label='Parent'
+                                control={
+                                  <Field
+                                    name='primary'
+                                    component={Radio}
+                                    type='radio'
+                                    value='parent'
+                                  />
+                                }
+                              />
+                              <FormControlLabel
+                                label='Carer'
+                                control={
+                                  <Field
+                                    name='primary'
+                                    component={Radio}
+                                    type='radio'
+                                    value='carer'
+                                  />
+                                }
+                              />
+                              <FormControlLabel
+                                label='Guardian'
+                                control={
+                                  <Field
+                                    name='primary'
+                                    component={Radio}
+                                    type='radio'
+                                    value='guardian'
+                                  />
+                                }
+                              />
+                            </RadioGroup>
+                          </FormControl>
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <div align='right'>
+                            <Button
+                              variant='contained'
+                              color='default'
+                              type='button'
+                              disabled={submitting}
+                              // endIcon={<ChevronRight />}
+                            >
+                              Remove adult
+                            </Button>
+                          </div>
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <Field
+                            fullWidth
+                            name='Role in the family '
+                            component={TextField}
+                            type='text'
+                            label='Role in the family'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <Field
+                            fullWidth
+                            required
+                            name='firstName'
+                            component={TextField}
+                            type='text'
+                            label='First Name'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <Field
+                            fullWidth
+                            required
+                            name='lastName'
+                            component={TextField}
+                            type='text'
+                            label='Last Name'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <FormControl component='fieldset'>
+                            <FormLabel component='legend'>Gender</FormLabel>
+                            <RadioGroup row>
+                              <FormControlLabel
+                                label='F'
+                                control={
+                                  <Field name='gender' component={Radio} type='radio' value='F' />
+                                }
+                              />
+                              <FormControlLabel
+                                label='M'
+                                control={
+                                  <Field name='gender' component={Radio} type='radio' value='M' />
+                                }
+                              />
+                              <FormControlLabel
+                                label='Other'
+                                control={
+                                  <Field
+                                    name='gender'
+                                    component={Radio}
+                                    type='radio'
+                                    value='Other'
+                                  />
+                                }
+                              />
+                            </RadioGroup>
+                          </FormControl>
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <MTextField
+                            fullWidth
+                            id='date'
+                            label='Date of birth'
+                            type='date'
+                            InputLabelProps={{
+                              shrink: true,
+                            }}
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <FormControl component='fieldset'>
+                            <FormGroup col>
+                              <FormLabel component='legend'>
+                                Do you identify as Aboriginal and/or Torres Strait Islander?
+                              </FormLabel>
+                              <FormControlLabel
+                                label='Aboriginal'
+                                control={
+                                  <Field
+                                    name='Aboriginal'
+                                    component={Checkbox}
+                                    type='checkbox'
+                                    value='Aboriginal'
+                                  />
+                                }
+                              />
+                              <FormControlLabel
+                                label='Torres Strait Islander'
+                                control={
+                                  <Field
+                                    name='Aboriginal'
+                                    component={Checkbox}
+                                    type='checkbox'
+                                    value='Torres Strait Islander'
+                                  />
+                                }
+                              />
+                            </FormGroup>
+                          </FormControl>
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <Field
+                            fullWidth
+                            name='Cultural identity'
+                            component={TextField}
+                            type='text'
+                            label='Cultural identity'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={8}>
+                          <Field
+                            fullWidth
+                            name='Language spoken at home'
+                            component={TextField}
+                            type='text'
+                            label='Language spoken at home'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={4}>
+                          <FormControlLabel
+                            label='Needs interpreter'
+                            control={
+                              <Field
+                                name='Needs interpreter'
+                                component={Checkbox}
+                                type='checkbox'
+                              />
+                            }
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={8}>
+                          <Field
+                            fullWidth
+                            name='Medicare number'
+                            component={TextField}
+                            type='text'
+                            label={
+                              <span>
+                                Medicare number <em>(if available)</em>
+                              </span>
+                            }
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={4}>
+                          <Field
+                            fullWidth
+                            name='Ref'
+                            component={TextField}
+                            type='text'
+                            label='Ref'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <Field
+                            fullWidth
+                            name='Centrelink'
+                            component={TextField}
+                            type='text'
+                            label={
+                              <span>
+                                Centrelink number <em>(CRN, if available)</em>
+                              </span>
+                            }
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={12} style={{ marginTop: 20 }}>
+                          <Typography variant='h5' component='h5'>
+                            Contact details
+                          </Typography>
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <Field
+                            fullWidth
+                            name='home-phone'
+                            component={TextField}
+                            label='Home phone'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <Field
+                            fullWidth
+                            name='mobile-phone'
+                            component={TextField}
+                            label='Mobile'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <Field
+                            name='email'
+                            fullWidth
+                            required
+                            component={TextField}
+                            type='email'
+                            label='Email'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <FormControlLabel
+                            label='Wish to receive a copy of this form via email'
+                            control={
+                              <Field
+                                name='Wish to receive a copy of this form via email'
+                                component={Checkbox}
+                                type='checkbox'
+                              />
+                            }
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                          <Field
+                            fullWidth
+                            name='address'
+                            component={TextField}
+                            label='Mailing address'
+                          />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                          <Field fullWidth name='Suburb' component={TextField} label='Suburb' />
+                        </Grid>
+
+                        <Grid item xs={12} md={3}>
+                          <Field
+                            fullWidth
+                            name='State'
+                            component={Select}
+                            label='Select a State'
+                            formControlProps={{ fullWidth: true }}
+                          >
+                            <MenuItem value='NSW'>NSW</MenuItem>
+                            <MenuItem value='VIC'>VIC</MenuItem>
+                            <MenuItem value='SA'>SA</MenuItem>
+                            <MenuItem value='WA'>WA</MenuItem>
+                            <MenuItem value='TAS'>TAS</MenuItem>
+                            <MenuItem value='QLD'>QLD</MenuItem>
+                          </Field>
+                        </Grid>
+
+                        <Grid item xs={12} md={3}>
+                          <Field fullWidth name='Postcode' component={TextField} label='Postcode' />
+                        </Grid>
+                      </Grid>
+                    </AccordionDetails>
+                  </Accordion>
                   <Grid item xs={12} md={12} style={{ marginTop: 10, marginBottom: 20 }}>
-                    <div align='right'>
+                    <div align='middle'>
                       <Button
                         variant='contained'
                         color='primary'
@@ -479,15 +818,17 @@ function App() {
                   </Grid>
 
                   <Accordion
-                    expanded={adultExpanded === 'panel2'}
-                    onChange={handleChange('panel2')}
+                    expanded={adultExpanded === 'panel3'}
+                    onChange={handleChange('panel3')}
                   >
                     <AccordionSummary
                       expandIcon={<ExpandMoreIcon />}
-                      aria-controls='panel2d-content'
-                      id='panel2d-header'
+                      aria-controls='panel3d-content'
+                      id='panel3d-header'
                     >
-                      <Typography>Child 1:</Typography>
+                      <Typography variant='h5' component='h4'>
+                        Child 1:
+                      </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                       <Grid container alignItems='flex-start' spacing={2}>
@@ -736,7 +1077,7 @@ function App() {
                   </Accordion>
 
                   <Grid item xs={12} md={12} style={{ marginTop: 10, marginBottom: 20 }}>
-                    <div align='right'>
+                    <div align='middle'>
                       <Button
                         variant='contained'
                         color='primary'
